edeed8f8077d367195626edcf6edf1d70f2be89e
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2019-05-04 19:42:04 +0300

    Remove server code

--- src/RaceState.h
+++ src/RaceState.h
@@ -5,26 +5,6 @@ enum class RaceState {
     READY, STEADY, RUNNING, FINISH, UNKNOWN,
 };
 
-void getRaceStateString(RaceState state, String &s) {
-    switch (state) {
-        case RaceState::READY:
-            s = "READY";
-            break;
-        case RaceState::STEADY:
-            s = "STEADY";
-            break;
-        case RaceState::RUNNING:
-            s = "RUNNING";
-            break;
-        case RaceState::FINISH:
-            s = "FINISH";
-            break;
-        default:
-            s = "UNKNOWN";
-            break;
-    }
-
-}
 
 RaceState parseRaceState(const char *command) {
     if (strcmp("READY", command) == 0) {
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,6 @@
 #include "config.h"
 
 
-#ifdef SERVER
-#include "server/LapsCounterServer.h"
-LapsCounterServer *device;
-#endif
-
 #ifdef ROBOT
 #include "robot/Robot.h"
 Robot *device;
@@ -18,10 +13,6 @@ Frame *device;
 #endif
 
 void setup() {
-#ifdef SERVER
-    Serial.begin(115200);
-    device = new LapsCounterServer(ssid, pass);
-#endif
 #ifdef ROBOT
     Serial.begin(115200);
     device = new Robot(ssid, pass);

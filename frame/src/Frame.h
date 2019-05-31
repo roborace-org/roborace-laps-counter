f8c91d9036ac51333cad3624b6ceb75c5c54cbdd
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2019-05-31 21:29:40 +0300

    Tune frame ir send delay

--- frame/src/Frame.h
+++ frame/src/Frame.h
@@ -10,9 +10,22 @@ public:
 
     void loop() {
         irsend.sendNEC(CODE, 32);
-        delay(20);
+        delay(28);
     }
 
 private:
     IRsend irsend;
 };
+
+/**
+100 - 7
+75 - 8
+50 - 10
+35 - 12
+32 - 12
+30 - 12
+28 - 13
+25 - 7
+20 - 7
+10 - 0
+ */
